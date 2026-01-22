8c324d55de83ec0a41f20cdebdd14b51bb97622b
eliveltonsf <elivelton.seven@gmail.com>
2026-01-22 12:36:37 -0300

    chore: :heavy_plus_sign: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-export default function Home() {
-  const teste = "";
-  return (
-    <div>
-      <h1 className="bg-red-500 text-2xl font-bold">Home</h1>
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        Submit
-      </button>
-    </div>
-  );
-}
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return <Button>wolrd</Button>;
+};
+
+export default Home;
