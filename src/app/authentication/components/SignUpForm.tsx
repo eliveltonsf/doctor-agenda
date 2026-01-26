87b123130b3cd19c8e927110e09c327cf869e6aa
eliveltonsf <elivelton.seven@gmail.com>
2026-01-26 14:04:10 -0300

    feat: :sparkles: add sign up form

---
+++ src/app/authentication/components/SignUpForm.tsx
@@ -0,0 +1,130 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+
+const registerSchema = z.object({
+  name: z.string().trim().min(2, "Nome é obrigatório"),
+  email: z.email("Formato de email inválido"),
+  password: z.string().trim().min(8, "A senha deve ter no mínimo 8 caracteres"),
+});
+
+const SignUpForm: React.FC = () => {
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof registerSchema>) {
+    console.log(data);
+  }
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Criar conta</CardTitle>
+        <CardDescription>Crie uma conta para continuar</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form
+          id="form-register"
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4"
+        >
+          <FieldGroup className="gap-3">
+            <Controller
+              name="name"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-register-name">Nome</FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-register-name"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Digite seu nome"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-register-email">Email</FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-register-email"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Digite seu email"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-register-password">
+                    Senha
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    type="password"
+                    id="form-register-password"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Digite sua senha"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Field orientation="horizontal">
+          <Button type="submit" form="form-register" className="w-full">
+            Submit
+          </Button>
+        </Field>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default SignUpForm;
